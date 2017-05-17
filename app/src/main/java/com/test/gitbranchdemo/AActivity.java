1604576cc55a816f6f0e10ff553cad8f27ed7b45
yeliangliang <yellcat@foxmail.com>
2017-05-17 14:25:46 +0800

    developer分支上的新功能已开发完成

--- app/src/main/java/com/test/gitbranchdemo/AActivity.java
+++ app/src/main/java/com/test/gitbranchdemo/AActivity.java
@@ -12,5 +12,7 @@ public class AActivity extends AppCompatActivity {
         //1. 新建工程，即master分支，先将其上传到git远程
         //2. 从master分支新建developer分支
         //3. 提交一次developer分支（为了便于观察）
+        //4. 正在改动developer分支上的代码的时候master分支出现了紧急bug
+
     }
 }
